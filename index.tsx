3f8d5b0c0695d28639a1a0f5a1cc3b2f7dfb1c2a
keumsukcho99-netizen <keumsukcho99@gmail.com>
2026-01-27 21:35:19 +0900

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-console.log("%c[Antique-Korea] 시스템 연결 성공", "color: #b8860b; font-weight: bold;");
-
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+const container = document.getElementById('root');
+if (container) {
+    const root = createRoot(container);
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
 }
